9e73db052498576750ac423eaf37a31cafb313f0
souravnitkkr1 <kumar.sourav@timesinternet.in>
2021-08-03 17:13:47 +0530

    Added annotation to Post.java

--- Backend/src/main/java/com/TILSocial/Backend/Model/Post.java
+++ Backend/src/main/java/com/TILSocial/Backend/Model/Post.java
@@ -1,7 +1,12 @@
 package com.TILSocial.Backend.Model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="post_DB")
 public class Post {
 	//empImgUrl,images,videos not used
+   @Id	
    private int id;
    private int empId;
    private String name;
